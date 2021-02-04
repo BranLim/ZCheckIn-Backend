7425232933812a1f5804e11255d2a79802565240
Brandon Lim <limws.brandon@gmail.com>
2021-02-04 22:15:43 +0800

    Add project files.

---
+++ RegisterUser.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace check_in_user_registration
+{
+    public static class RegisterUser
+    {
+        [FunctionName("RegisterUser")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Registering User.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            if (string.IsNullOrEmpty(requestBody))
+            {
+                return new BadRequestObjectResult("Invalid request body");
+            }
+
+            RegisterUserRequest data = JsonConvert.DeserializeObject<RegisterUserRequest>(requestBody);
+
+            string uuid = data?.UUID;
+
+            if (string.IsNullOrEmpty(uuid))
+            {
+                return new BadRequestObjectResult("missing UUID");
+            }
+
+            string faceImage = data?.FaceImageAsBase64;
+
+
+
+
+            return new OkObjectResult(string.Format("Your ID is {0}", uuid));
+        }
+    }
+}
---
+++ RegisterUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace check_in_user_registration
+{
+    public class RegisterUserRequest
+    {
+        public RegisterUserRequest()
+        {
+        }
+
+        public string UUID { get; set; }
+
+        public string FaceImageAsBase64 { get; set; }
+    }
+}
