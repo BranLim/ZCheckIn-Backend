04874c5689bf1f56c5323ce3363d054994139aaa
Brandon Lim <limws.brandon@gmail.com>
2021-02-07 22:51:10 +0800

    Add support for email notification

--- ProcessUploadedUser.cs
+++ ProcessUploadedUser.cs
@@ -1,17 +1,62 @@
 using System;
 using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ZCheckIn.Backend.DTOs;
 
 namespace ZCheckIn_Backend
 {
     public static class ProcessUploadedUser
     {
         [FunctionName("ProcessUploadedUser")]
-        public static void Run([BlobTrigger("user/{uuid}", Connection = "Azurite_Storage")]Stream myBlob, string uuid, ILogger log)
+        public static void Run([BlobTrigger("users/user/{uuid}", Connection = "Azurite_Storage")] Stream myBlob, string uuid, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{uuid} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation(string.Format("User {0} added to storage. Processing...", uuid));
+
+            string jsonContent = new StreamReader(myBlob).ReadToEnd();
+            UserDTO user = JsonConvert.DeserializeObject<UserDTO>(jsonContent);
+
+            string content = "";
+
+            log.LogInformation("Sending email notification.");
+
+            //SendEmail(log, content);
+
+            log.LogInformation("Email sent.");
+
+        }
+
+        private static void SendEmail(ILogger log, string emailContent)
+        {
+            try
+            {
+
+                MailMessage mailMsg = new MailMessage();
+                mailMsg.To.Add(new MailAddress("", "Admin@ZCheckIn"));
+                mailMsg.From = new MailAddress("", "ZCheckIn App");
+
+                mailMsg.Subject = "New User/Employee";
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(emailContent, null, MediaTypeNames.Text.Html));
+
+
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("<SendGrid-username>", "<SendGrid-password>");
+                smtpClient.Credentials = credentials;
+                smtpClient.Send(mailMsg);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(string.Format("Error sending email. Error: {0}", ex.Message));
+            }
+
+
         }
     }
+
+
 }
